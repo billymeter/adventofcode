a022502b662a645ce91127436f2657df9cac81a3
Brad Trotter <billymeter@gmail.com>
2018-12-13 21:24:51 -0500

    day 4 done

--- 04/sol.py
+++ 04/sol.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import re
+import operator
 from datetime import datetime
+from functools import reduce
 from collections import defaultdict
 
 def get_lines(fname):
@@ -12,37 +14,64 @@ def parse_line(line):
     pass
 
 def parse(data):
-    # [1518-03-10 23:56] Guard #443 begins shift
-    # [1518-03-11 00:22] falls asleep
-    # [1518-03-11 00:27] wakes up
     guards = defaultdict(dict)
-    it = iter(data)
     current_guard = 0
+    it = iter(data)
 
     for line in it:
         if not line: continue
         if "shift" in line:
             m = re.search(".+ #(\d+).*", line)
-            if m: current_guard = m.groups()[0]
+            if m:
+                current_guard = m.groups()[0]
+                if 'sleep' not in guards[current_guard].keys():
+                    guards[current_guard] = defaultdict(list)
         if "asleep" in line:
-            m = re.search("[(.+)]", line)
+            m = re.search("\[(.*)\]", line)
             if m:
-                print("re found sleep")
-                date = datetime.strptime(m.groups()[0], "%Y-%m-%d %H:%M")
-                guards[current_guard]['sleep'].append(date)
+                sleep = datetime.strptime(m.groups()[0], "%Y-%m-%d %H:%M")
+                guards[current_guard]['sleep'].append(sleep)
             next_line = next(it)
-            m = re.search("[(.+)]", next_line)
+            m = re.search("\[(.+)\]", next_line)
             if m:
-                print("re found wake")
-                date = datetime.strptime(m.groups()[0], "%Y-%m-%d %H:%M")
-                guards[current_guard]['wake'].append(date)
-
+                wake = datetime.strptime(m.groups()[0], "%Y-%m-%d %H:%M")
+                guards[current_guard]['wake'].append(wake)
+                guards[current_guard]['sleep_seconds'].append((wake - sleep).total_seconds())
     return guards
+
+def most_time_asleep(guards):
+    sleep = defaultdict(dict)
+
+    for k in guards:
+        sleep[k]['sleep'] = reduce(operator.add, guards[k]['sleep_seconds'], 0)
+        sleep[k]['mins'] = []
+        for min in zip(guards[k]['sleep'], guards[k]['wake']):
+            sleep[k]['mins'] += [x for x in range(min[0].minute, min[1].minute)]
+    return sorted(sleep.items(), key=lambda x: x[1]['sleep'], reverse=True)
+
+def part1(guards):
+    gid = int(guards[0][0])
+    return gid * max(guards[0][1]['mins'], key=guards[0][1]['mins'].count)
+
+def part2(guards):
+    max_min, max_count, gid = 0, 0, 0
+    for g in guards:
+        if not g[1]['mins']: continue
+        d = defaultdict(int)
+        for m in g[1]['mins']: d[m] += 1
+        test = max(d.items(), key=lambda x: x[1])
+        if test[1] > max_count:
+            max_count = test[1]
+            max_min = test[0]
+            gid = int(g[0])
+    return gid * max_min
 
 if __name__ == "__main__":
     # sort = sorted(get_lines("input"))
     # with open("input_sorted", "w") as f:
     #     f.write('\n'.join(sort))
     data = get_lines("input_sorted")
-    print(data[1:4])
-    print(parse(data[4]))
+    guards = parse(data)
+    sleep_data = most_time_asleep(guards)
+    print(part1(sleep_data))
+    print(part2(sleep_data))
