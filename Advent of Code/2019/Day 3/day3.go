e4c4971a107bbf6ea5ea0dfe536556f80dab3b31
bt <bt@whitehatters.org>
2019-12-04 07:49:07 -0500

    started 2019 d3

--- Advent of Code/2019/Day 3/day3.go
+++ Advent of Code/2019/Day 3/day3.go
@@ -1,5 +1,73 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+type trool struct {
+	// lame attempt at a boolean-like value but with three
+	// states
+	val int
+}
+
+var (
+	none = trool{0}
+	one  = trool{1}
+	both = trool{2}
+)
+
+type point struct {
+	x int
+	y int
+}
+
+type operation struct {
+	direction byte
+	magnitude int
+}
+
+func main() {
+	input := readInput("input.txt")
+	fmt.Println(input)
+}
+
+func drawLine(o operation, start point, grid map[point]trool) {
+	switch o.direction {
+	case 'U':
+		for y := start.y; y <= o.magnitude; y++ {
+
+		}
+	}
+}
+
+func parseLine(s string) []operation {
+	moves := strings.Split(s, ",")
+	var ops []operation
+	for _, m := range moves {
+		direction := m[0]
+		magnitude, _ := strconv.Atoi(m[1:])
+		ops = append(ops, operation{direction, magnitude})
+	}
+	return ops
+}
+
+func readInput(filename string) [][]operation {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ops [][]operation
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		l := s.Text()
+		ops = append(ops, parseLine(l))
+	}
+
+	return ops
 }
