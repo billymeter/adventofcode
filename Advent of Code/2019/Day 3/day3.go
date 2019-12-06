c3a4c15b9f7f7f26b55d82eb8d67130db3fb9940
bt <bt@whitehatters.org>
2019-12-05 19:16:37 -0500

    2019 d3p1 almost done

--- Advent of Code/2019/Day 3/day3.go
+++ Advent of Code/2019/Day 3/day3.go
@@ -5,20 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
-)
-
-type trool struct {
-	// lame attempt at a boolean-like value but with three
-	// states
-	val int
-}
-
-var (
-	none = trool{0}
-	one  = trool{1}
-	both = trool{2}
 )
 
 type point struct {
@@ -32,17 +21,87 @@ type operation struct {
 }
 
 func main() {
-	input := readInput("input.txt")
-	fmt.Println(input)
+	// input := readInput("input.txt")
+	input := readInput("test.txt")
+
+	cursor := point{0, 0}
+	grid := make(map[point]int)
+	for i, l := range input {
+		fmt.Println(l)
+		for _, o := range l {
+			cursor = drawLine(o, cursor, i, grid)
+		}
+		cursor = point{0, 0}
+	}
+
+	intersections := findIntersections(grid)
+
+	fmt.Println("intersections:", intersections)
+
+	var distances []int
+	for _, i := range intersections {
+		distances = append(distances, calculateManhattanDistance(i))
+	}
+	sort.Ints(distances)
+	fmt.Println(distances)
 }
 
-func drawLine(o operation, start point, grid map[point]trool) {
-	switch o.direction {
-	case 'U':
-		for y := start.y; y <= o.magnitude; y++ {
+func calculateManhattanDistance(p point) int {
+	centralPort := point{0, 0}
+	val := (p.x - centralPort.x) + (p.y - centralPort.y)
+	if val < 0 {
+		val *= -1
+	}
+	return val
+}
 
+func findIntersections(grid map[point]int) []point {
+	var candidates []point
+	for k, v := range grid {
+		if v > 2 {
+			// if v&3 == 3 {
+			fmt.Println(k, "has a value of", v)
+			candidates = append(candidates, k)
 		}
 	}
+	return candidates
+}
+
+func drawLine(o operation, start point, wire int, grid map[point]int) point {
+	// 0 for no wire in path, 1 for one wire, and 11 for two wires
+	shift := 1 << wire
+	switch o.direction {
+	case 'U':
+		for y := 1; y < o.magnitude; y++ {
+			p := point{start.x, start.y + y}
+			fmt.Println("Point", p, "marked for wire", wire+1)
+			grid[p] |= shift
+		}
+		return point{start.x, start.y + o.magnitude}
+	case 'D':
+		for y := 1; y < o.magnitude; y++ {
+			p := point{start.x, start.y - y}
+			fmt.Println("Point", p, "marked for wire", wire+1)
+			grid[p] |= shift
+		}
+		return point{start.x, start.y - o.magnitude}
+	case 'R':
+		for x := 1; x < o.magnitude; x++ {
+			p := point{start.x + x, start.y}
+			fmt.Println("Point", p, "marked for wire", wire+1)
+			grid[p] |= shift
+		}
+		return point{start.x + o.magnitude, start.y}
+	case 'L':
+		for x := 1; x < o.magnitude; x++ {
+			p := point{start.x - x, start.y}
+			fmt.Println("Point", p, "marked for wire", wire+1)
+			grid[p] |= shift
+		}
+		return point{start.x - o.magnitude, start.y}
+	}
+	fmt.Println("i came into a bad area")
+	return point{0, 0}
 }
 
 func parseLine(s string) []operation {
