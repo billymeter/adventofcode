95fdc9ef8e6d0714e5fa144920b1f3ba8a6ebbd7
Brad Trotter <bradley.trotter.efx@gmail.com>
2021-12-08 15:43:28 -0500

    2021 day4 in progress

--- Advent of Code/2021/day4.py
+++ Advent of Code/2021/day4.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+from functools import reduce
+import operator
+
 
 class Board:
     def __init__(self, lines):
-        self.board = [x.split('\n') for x in lines.split('\n')]
+        self.board = [[*map(int, x.split('\n')[0].split())]
+                      for x in lines.split('\n')]
+        self.numbers = []
+        self.i = 0
         pass
 
     def __str__(self):
@@ -12,85 +18,76 @@ class Board:
     def __repr__(self):
         return str(self.board)
 
-    def check_for_win(self):
-        pass
+    def __iter__(self):
+        return self
 
-    def mark_number(self):
-        pass
+    def __next__(self):
+        if self.i < len(self.board):
+            self.i += 1
+            return self.board[self.i - 1]
+        else:
+            raise StopIteration
+
+    def check_for_win(self):
+        # check for horizontal lines first
+        for line in self.board:
+            wins = 0
+            for num in line:
+                if num in self.numbers:
+                    wins += 1
+                if wins == 5:
+                    return line
+
+        # transpose the board to easily check
+        # the vertical lines
+        self.board = [[row[i] for row in self.board] for i in range(5)]
+
+        for line in self.board:
+            wins = 0
+            for num in line:
+                if num in self.numbers:
+                    wins += 1
+                if wins == 5:
+                    return line
+
+        # transpose the board again to
+        # restore the original board
+        self.board = [[row[i] for row in self.board] for i in range(5)]
+
+        return None
+
+    def mark_number(self, number):
+        self.numbers.append(number)
 
 
 def build_boards(data):
     temp = data.split('\n\n')
-    numbers = temp[0]
+    numbers = [*map(int, temp[0].split(','))]
     board_data = temp[1:]
     boards = []
     for board in board_data:
         boards.append(Board(board))
-    
+
     return numbers, boards
 
-    
 
 with open('input4_sample.txt') as f:
-    # numbers = [int(x.strip()) for x in f.readline().split(',')]
-
-    # data = [x.strip() for x in f.read().split('\n\n')]
-    # boards = []
-    # for board in data:
-    #     lines = [x.split() for x in board.split('\n')]
-    #     bd = []
-    #     for line in lines:
-    #         bd.append([[int(x), False] for x in line])
-    #     boards.append(bd)
     data = f.read()
 
 
-print(build_boards(data))
+def day1():
+    numbers, boards = build_boards(data)
+    for num in numbers:
+        for board in boards:
+            board.mark_number(num)
+            win = board.check_for_win()
+            if win:
+                # get all the lines that are not the winning line
+                d = [x for x in board if x != win]
+                # flatten the list and get its sum
+                sum = reduce(operator.add, [
+                             x for sublist in d for x in sublist])
+                return sum * num
 
 
-# def mark_number(num):
-#     print(f'marking {num}')
-#     for board in boards:
-#         for line in board:
-#             for number in line:
-#                 if number[0] == num:
-#                     number[1] = True
-#                     return
-
-# def check_boards():
-#     # check horizontal lines
-#     for board in boards:
-#         for line in board:
-#             win = True
-#             for _, status in line:
-#                 if not status:
-#                     win = False
-#                     continue
-#             if win:
-#                 return line
-#     return None
-#     # check vertical lines
-#     # for board in boards:
-        
-
-
-# def day1():
-#     guesses = 0
-#     for number in numbers:
-#         guesses += 1
-#         mark_number(number)
-#         if guesses >= 5:
-#             w = check_boards()
-#             if w:
-#                 return w
-
-
-# def day2():
-#     pass
-
-
-# # print(numbers)
-# # print(boards)
-# print(day1())
-# print(boards)
-# # print(day2())
+print(day1())
