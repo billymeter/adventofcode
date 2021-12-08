c81daa4c54527b09b8886531d985596dd130fc09
Brad <bt@whitehatters.org>
2021-12-08 08:37:10 -0500

    2021 day4 still in progress

--- Advent of Code/2021/day4.py
+++ Advent of Code/2021/day4.py
@@ -1,31 +1,96 @@
 #!/usr/bin/env python3
 
-with open('input4.txt') as f:
-    def remove_empty(x):
-        if x == '':
-            return True
-        return False
-    numbers = [int(x.strip()) for x in f.readline().split(',')]
 
-    data = [filter(remove_empty, x.split('\n'))
-            for x in f.read().split('\n\n')]
+class Board:
+    def __init__(self, lines):
+        self.board = [x.split('\n') for x in lines.split('\n')]
+        pass
+
+    def __str__(self):
+        return str(self.board)
+
+    def __repr__(self):
+        return str(self.board)
+
+    def check_for_win(self):
+        pass
+
+    def mark_number(self):
+        pass
+
+
+def build_boards(data):
+    temp = data.split('\n\n')
+    numbers = temp[0]
+    board_data = temp[1:]
     boards = []
-    for x in data:
-        b = []
-        for y in x:
-            b.append(y)
-        boards.append(x)
+    for board in board_data:
+        boards.append(Board(board))
+    
+    return numbers, boards
+
+    
+
+with open('input4_sample.txt') as f:
+    # numbers = [int(x.strip()) for x in f.readline().split(',')]
+
+    # data = [x.strip() for x in f.read().split('\n\n')]
+    # boards = []
+    # for board in data:
+    #     lines = [x.split() for x in board.split('\n')]
+    #     bd = []
+    #     for line in lines:
+    #         bd.append([[int(x), False] for x in line])
+    #     boards.append(bd)
+    data = f.read()
 
 
-def day1():
-    pass
+print(build_boards(data))
 
 
-def day2():
-    pass
+# def mark_number(num):
+#     print(f'marking {num}')
+#     for board in boards:
+#         for line in board:
+#             for number in line:
+#                 if number[0] == num:
+#                     number[1] = True
+#                     return
+
+# def check_boards():
+#     # check horizontal lines
+#     for board in boards:
+#         for line in board:
+#             win = True
+#             for _, status in line:
+#                 if not status:
+#                     win = False
+#                     continue
+#             if win:
+#                 return line
+#     return None
+#     # check vertical lines
+#     # for board in boards:
+        
 
 
-print(numbers)
-print(boards)
-print(day1())
-print(day2())
+# def day1():
+#     guesses = 0
+#     for number in numbers:
+#         guesses += 1
+#         mark_number(number)
+#         if guesses >= 5:
+#             w = check_boards()
+#             if w:
+#                 return w
+
+
+# def day2():
+#     pass
+
+
+# # print(numbers)
+# # print(boards)
+# print(day1())
+# print(boards)
+# # print(day2())
