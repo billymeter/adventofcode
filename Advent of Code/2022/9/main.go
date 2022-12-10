37ea1e13a4b63a26150d227c86fa67e9fbf76170
Brad Trotter <billymeter@gmail.com>
2022-12-10 13:36:57 -0500

    completed day 9

--- Advent of Code/2022/9/main.go
+++ Advent of Code/2022/9/main.go
@@ -76,74 +76,91 @@ func makeTailAdjacentToHead(head, tail Point) Point {
 		return tail
 	}
 
-	var t Point
 	// 8 possibilities
 	// lagging from the right
 	if head.x == tail.x-2 && head.y == tail.y {
-		t = Point{tail.x - 1, tail.y}
+		return Point{tail.x - 1, tail.y}
 	}
 	// lagging from the left
 	if head.x == tail.x+2 && head.y == tail.y {
-		t = Point{tail.x + 1, tail.y}
+		return Point{tail.x + 1, tail.y}
 	}
 
 	// lagging from the top
 	if head.x == tail.x && head.y == tail.y+2 {
-		t = Point{tail.x, tail.y + 1}
+		return Point{tail.x, tail.y + 1}
 	}
 	// lagging from the bottom
 	if head.x == tail.x && head.y == tail.y-2 {
-		t = Point{tail.x, tail.y - 1}
+		return Point{tail.x, tail.y - 1}
 	}
 
 	// lagging from the lower diagonals
 	if head.x == tail.x-1 && head.y == tail.y+2 || head.x == tail.x+1 && head.y == tail.y+2 {
-		t = Point{head.x, tail.y + 1}
+		return Point{head.x, tail.y + 1}
 	}
 
 	// lagging from the upper diagonals
 	if head.x == tail.x-1 && head.y == tail.y-2 || head.x == tail.x+1 && head.y == tail.y-2 {
-		t = Point{head.x, tail.y - 1}
+		return Point{head.x, tail.y - 1}
 	}
 
 	// lagging from the right diagonals
 	if head.x == tail.x-2 && head.y == tail.y+1 || head.x == tail.x-2 && head.y == tail.y-1 {
-		t = Point{tail.x - 1, head.y}
+		return Point{tail.x - 1, head.y}
 	}
 
 	// lagging from the left diagonals
 	if head.x == tail.x+2 && head.y == tail.y+1 || head.x == tail.x+2 && head.y == tail.y-1 {
-		t = Point{tail.x + 1, head.y}
+		return Point{tail.x + 1, head.y}
 	}
+
+	if head.x == tail.x-2 && head.y == tail.y-2 {
+		return Point{tail.x - 1, tail.y - 1}
+	}
+	if head.x == tail.x-2 && head.y == tail.y+2 {
+		return Point{tail.x - 1, tail.y + 1}
+	}
+	if head.x == tail.x+2 && head.y == tail.y-2 {
+		return Point{tail.x + 1, tail.y - 1}
+	}
+	if head.x == tail.x+2 && head.y == tail.y+2 {
+		return Point{tail.x + 1, tail.y + 1}
+	}
+
+	t := Point{-100, -100}
 	return t
 }
 
-func simulateRopeMoves(moves []string) map[Point]struct{} {
-	head := Point{0, 0}
-	tail := Point{0, 0}
+func simulateRopeMoves(moves []string, numKnots int) map[Point]struct{} {
+	var knots []Point
+	for i := 0; i < numKnots; i++ {
+		knots = append(knots, Point{0, 0})
+	}
+
 	visited := make(map[Point]struct{})
-	visited[tail] = struct{}{}
 
 	for _, m := range moves {
 		component := strings.Split(m, " ")
 		amount, _ := strconv.Atoi(component[1])
 		for i := 0; i < amount; i++ {
-			// fmt.Printf("h: %v, t: %v --> ", head, tail)
 			switch component[0] {
 			case "D":
-				head.y--
+				knots[0].y--
 			case "L":
-				head.x--
+				knots[0].x--
 			case "R":
-				head.x++
+				knots[0].x++
 			case "U":
-				head.y++
+				knots[0].y++
 			default:
 				log.Fatal("unrecognized move", component[0])
 			}
-			tail = makeTailAdjacentToHead(head, tail)
-			// fmt.Printf("h: %v, t: %v\n", head, tail)
-			visited[tail] = struct{}{}
+
+			for i := 1; i < numKnots; i++ {
+				knots[i] = makeTailAdjacentToHead(knots[i-1], knots[i])
+			}
+			visited[knots[len(knots)-1]] = struct{}{}
 		}
 	}
 
@@ -152,13 +169,15 @@ func simulateRopeMoves(moves []string) map[Point]struct{} {
 
 func day9p1() {
 	moves := readInput("input")
-	visited := simulateRopeMoves(moves)
+	visited := simulateRopeMoves(moves, 2)
+
 	fmt.Printf("%d\n", len(visited))
 }
 
 func day9p2() {
-	moves := readInput("test")
-	visited := simulateRopeMoves(moves)
+	moves := readInput("input")
+	visited := simulateRopeMoves(moves, 10)
+
 	fmt.Printf("%d\n", len(visited))
 }
 
